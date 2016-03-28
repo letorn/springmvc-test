3559c5e8326204d3f934d72b2d0dfa625bd94d98
Weitang.Li <letorn@163.com>
2016-03-28 13:08:49 +0800

    o

--- src/test/ActiveMQTest.java
+++ src/test/ActiveMQTest.java
@@ -1,6 +1,7 @@
 package test;
 
 import javax.jms.JMSException;
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.Session;
 
@@ -21,9 +22,62 @@ public class ActiveMQTest {
 
 	@Test
 	public void test() {
-		jmsTemplate.send(jmsTemplate.getDefaultDestination(), new MessageCreator() {
+		jmsTemplate.send(new MessageCreator() {
 			public Message createMessage(Session session) throws JMSException {
-				return session.createTextMessage("something");
+				Message message = session.createMessage();
+				message.setStringProperty("name", "test");
+				message.setStringProperty("content", "i am a message !");
+				return message;
+			}
+		});
+	}
+	
+	@Test
+	public void testTextMessage() {
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("i am a text message !");
+			}
+		});
+	}
+	
+	@Test
+	public void testMapMessage() {
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				MapMessage message = session.createMapMessage();
+				message.setString("name", "test");
+				message.setString("content", "i am a map message !");
+				return message;
+			}
+		});
+	}
+	
+	@Test
+	public void testMessages() {
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("i am a message 1 !");
+			}
+		});
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("i am a message 2 !");
+			}
+		});
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("i am a message 3 !");
+			}
+		});
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("i am a message 4 !");
+			}
+		});
+		jmsTemplate.send(new MessageCreator() {
+			public Message createMessage(Session session) throws JMSException {
+				return session.createTextMessage("i am a message 5 !");
 			}
 		});
 	}
--- src/test/JMSListener.java
+++ src/test/JMSListener.java
@@ -1,19 +1,65 @@
 package test;
 
-import javax.jms.JMSException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Random;
+
+import javax.jms.MapMessage;
 import javax.jms.Message;
 import javax.jms.MessageListener;
 import javax.jms.TextMessage;
 
 public class JMSListener implements MessageListener {
 
+	private DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+	private Random random = new Random();
+
 	public void onMessage(Message message) {
-		TextMessage tm = (TextMessage) message;
 		try {
-			System.out.println("ConsumerMessageListener收到了文本消息：\t"
-					+ tm.getText());
-		} catch (JMSException e) {
+			Date beginDate = new Date();
+			Thread.sleep(random.nextInt(5) * 1000);
+			Date endDate = new Date();
+			System.out.println();
+			System.out.println("--------------------" + dateFormat.format(beginDate) + "  " + Thread.currentThread());
+			handleMessage(message);
+			System.out.println("--------------------" + dateFormat.format(endDate) + "--------------------");
+			System.out.println();
+		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+	private void handleMessage(Message message) throws Exception {
+		if (message instanceof MapMessage) {
+			MapMessage mapMessage = (MapMessage) message;
+			Enumeration<String> mapNames = mapMessage.getMapNames();
+			StringBuilder buf = new StringBuilder("ActiveMQ收到了消息: ");
+			while (mapNames.hasMoreElements()) {
+				String mapName = mapNames.nextElement();
+				String mapValue = mapMessage.getString(mapName);
+				buf.append(mapName + "=" + mapValue + ", ");
+			}
+			int lastSeparatorIndex = buf.lastIndexOf(",");
+			if (lastSeparatorIndex > -1)
+				buf.replace(lastSeparatorIndex, lastSeparatorIndex + 2, "");
+			System.out.println(buf.toString());
+		} else if (message instanceof TextMessage) {
+			TextMessage textMessage = (TextMessage) message;
+			System.out.println("ActiveMQ收到了消息: " + textMessage.getText());
+		} else {
+			Enumeration<String> mapNames = message.getPropertyNames();
+			StringBuilder buf = new StringBuilder("ActiveMQ收到了消息: ");
+			while (mapNames.hasMoreElements()) {
+				String mapName = mapNames.nextElement();
+				String mapValue = message.getStringProperty(mapName);
+				buf.append(mapName + "=" + mapValue + ", ");
+			}
+			int lastSeparatorIndex = buf.lastIndexOf(",");
+			if (lastSeparatorIndex > -1)
+				buf.replace(lastSeparatorIndex, lastSeparatorIndex + 2, "");
+			System.out.println(buf.toString());
 		}
 	}
 
